5550fc2aace1afe64af7ae7aae1405a3b7c80e99
khaleds <khaled.abodaif@yahoo.com>
2023-03-12 16:32:22 +0200

    add casts and guard to implementation

--- src/Models/VoucherImplementation.php
+++ src/Models/VoucherImplementation.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class VoucherImplementation extends Model
 {
     use HasFactory;
+    
+    protected $casts=["value"=>"array",'name'=>'array'];
+    protected $guarded=['id'];
 }
